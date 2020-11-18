79302a717952a777bae58a1ad474b43d498960c5
Oleh Prypin <oleh@pryp.in>
2020-11-18 19:23:21 +0100

    Add CI

---
+++ .tools/ci.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+cd "$(dirname "$0")/.."
+
+with_groups() {
+    echo "::group::$@"
+    "$@" && echo "::endgroup::"
+}
+
+"$@" autoflake -i -r --remove-all-unused-imports --remove-unused-variables pytest_golden tests
+"$@" isort -q pytest_golden tests
+"$@" black -q pytest_golden tests
+"$@" pytest -q
+python -c 'import sys, os; sys.exit((3,8) <= sys.version_info < (3,9) and os.name == "posix")' ||
+"$@" pytype pytest_golden
+PYTHONPATH=$(pwd)/example "$@" pytest -q example
