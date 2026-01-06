oprypin/pytest-golden

commit 02c51c35270dd7f797505a67b35e833d42ce6d80

Update template, fix lints

pytest_golden/plugin.py
@@ -6,7 +6,8 @@ import inspect
 import logging
 import pathlib
 import warnings
-from typing import TYPE_CHECKING, Any, Callable, Collection, Sequence, TypeVar
+from collections.abc import Collection, Sequence
+from typing import TYPE_CHECKING, Any, Callable, TypeVar
 
 import atomicwrites
 import pytest
@@ -16,7 +17,8 @@ from . import yaml
 if TYPE_CHECKING:
     import os
 
-    T = TypeVar("T")
+
+T = TypeVar("T")
 
 
 def pytest_addoption(parser):
@@ -360,7 +362,7 @@ def pytest_generate_tests(metafunc) -> None:
     rel_paths = [path.relative_to(directory) for path in paths]
     skip_parts = None
     if all(
-        _removeprefix("test_", path.parts[0]) == _removeprefix("test_", item.originalname)
+        "test_".removeprefix(path.parts[0]) == "test_".removeprefix(item.originalname)
         for path in rel_paths
     ):
         skip_parts = 1
@@ -372,12 +374,6 @@ def pytest_generate_tests(metafunc) -> None:
         ids=ids,
         indirect=True,
     )
-
-
-def _removeprefix(prefix: str, s: str):
-    if s.startswith(prefix):
-        s = s[len(prefix) :]
-    return s
 
 
 def pytest_assertion_pass(item, lineno, orig, expl):

.github/workflows/ci.yml
@@ -13,19 +13,19 @@ jobs:
       fail-fast: false
       matrix:
         include:
-          - python: '^3.12'
+          - python: '^3.13'
             os: ubuntu-latest
+          - python: '3.13'
+            os: macos-latest
           - python: '3.12'
-            os: macos-latest
+            os: windows-latest
           - python: '3.11'
-            os: windows-latest
-          - python: '3.10'
             os: ubuntu-latest
-          - python: '3.9'
+          - python: '3.10'
             os: macos-latest
-          - python: '3.8'
+          - python: '3.9'
             os: windows-latest
-          - python: '3.8'
+          - python: '3.9'
             os: ubuntu-latest
             versions: minimal
     runs-on: ${{matrix.os}}

.tools/copier-answers.yml
@@ -1,4 +1,4 @@
-_commit: 0903da199
+_commit: ef9641fa3
 _src_path: gh:oprypin/py-project-template
 copyright_date: '2020'
 mkdocs: false

pyproject.toml
@@ -18,16 +18,16 @@ classifiers = [
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
     "Topic :: Software Development :: Testing :: Unit",
     "Typing :: Typed",
 ]
 dynamic = ["version"]
-requires-python = ">=3.8"
+requires-python = ">=3.9"
 dependencies = [
     "pytest >=6.1.2",
     "ruamel.yaml >=0.16.12, <1.0",
@@ -96,29 +96,31 @@ line-length = 100
 [tool.ruff.lint]
 preview = true
 select = [
-    "F", "E", "W", "I", "UP", "YTT", "C4", "DTZ", "T10", "FA", "ISC", "PIE", "T20", "RSE", "TCH",
+    "F", "E", "W", "I", "UP", "YTT", "FBT", "C4", "DTZ", "T10", "FA", "ISC", "PIE", "T20", "RSE", "TCH",
     "N803", "N804", "N805", "N806", "N807", "N815", "N816", "N999",
-    "B002", "B003", "B005", "B007", "B008", "B009", "B010", "B011", "B012", "B013", "B014", "B015", "B016", "B017", "B018", "B020", "B021", "B022", "B023", "B025", "B026", "B029", "B030", "B031", "B032", "B033", "B034", "B035", "B905",
+    "B002", "B003", "B004", "B005", "B007", "B008", "B009", "B010", "B011", "B012", "B013", "B014", "B015", "B016", "B017", "B018", "B019", "B020", "B021", "B022", "B023", "B025", "B026", "B029", "B030", "B031", "B032", "B033", "B034", "B035", "B039", "B905", "B909",
     "COM818",
-    "G001", "G010", "G202",
+    "LOG001", "LOG007", "LOG009",
+    "G001", "G002", "G003", "G010", "G101", "G201", "G202",
     "Q004",
-    "RET502", "RET504",
-    "SIM101", "SIM103", "SIM105", "SIM107", "SIM118", "SIM201", "SIM202", "SIM208", "SIM210", "SIM211", "SIM212", "SIM220", "SIM221", "SIM222", "SIM223", "SIM300", "SIM401", "SIM910", "SIM911",
+    "RET502", "RET503", "RET504",
+    "SIM101", "SIM103", "SIM105", "SIM107", "SIM109", "SIM110", "SIM113", "SIM114", "SIM118", "SIM201", "SIM202", "SIM208", "SIM210", "SIM211", "SIM212", "SIM220", "SIM221", "SIM222", "SIM223", "SIM300", "SIM401", "SIM910", "SIM911",
+    "PTH201",
+    "TD004", "TD005", "TD007",
     "PGH003", "PGH004", "PGH005",
-    "PLC0105", "PLC0131", "PLC0132", "PLC0205", "PLC0208", "PLC0414", "PLC2401", "PLC2403", "PLC2701", "PLC2801", "PLC3002",
-    "PLE0100", "PLE0101", "PLE0116", "PLE0117", "PLE0118", "PLE0241", "PLE0302", "PLE0307", "PLE0604", "PLE0605", "PLE0704", "PLE1132", "PLE1142", "PLE1205", "PLE1206", "PLE1300", "PLE1307", "PLE1310", "PLE1507", "PLE1700", "PLE2502", "PLE2510", "PLE2512", "PLE2513", "PLE2514", "PLE2515",
-    "PLR0124", "PLR0133", "PLR0206", "PLR0402", "PLR1701", "PLR1722", "PLR1733", "PLR1736",
-    "PLW0108", "PLW0120", "PLW0127", "PLW0129", "PLW0131", "PLW0245", "PLW0406", "PLW0602", "PLW0603", "PLW0604", "PLW0711", "PLW1501", "PLW1508", "PLW1514", "PLW2101", "PLW3301",
-    "TRY302", "TRY401",
+    "PLC0105", "PLC0131", "PLC0132", "PLC0205", "PLC0206", "PLC0208", "PLC0414", "PLC2401", "PLC2403", "PLC2701", "PLC2801", "PLC3002",
+    "PLR0124", "PLR0133", "PLR0202", "PLR0203", "PLR0206", "PLR0402", "PLR1716", "PLR1722", "PLR1733", "PLR1736", "PLR2044", "PLR6301",
+    "TRY201", "TRY203", "TRY401",
     "FLY002",
     "PERF101", "PERF102", "PERF402", "PERF403",
-    "FURB105", "FURB118", "FURB131", "FURB132", "FURB148", "FURB163", "FURB167", "FURB168", "FURB169",
-    "LOG001", "LOG009",
-    "RUF001", "RUF005", "RUF007", "RUF008", "RUF009", "RUF010", "RUF013", "RUF015", "RUF016", "RUF017", "RUF018", "RUF019", "RUF020", "RUF021", "RUF022", "RUF100", "RUF200",
+    "FURB101", "FURB103", "FURB105", "FURB110", "FURB116", "FURB118", "FURB129", "FURB131", "FURB132", "FURB136", "FURB145", "FURB148", "FURB154", "FURB156", "FURB157", "FURB161", "FURB163", "FURB164", "FURB166", "FURB167", "FURB168", "FURB169", "FURB171", "FURB177", "FURB180", "FURB181", "FURB188", "FURB192",
+    "RUF001", "RUF002", "RUF003", "RUF005", "RUF006", "RUF007", "RUF008", "RUF009", "RUF010", "RUF012", "RUF013", "RUF015", "RUF016", "RUF017", "RUF018", "RUF019", "RUF020", "RUF021", "RUF022", "RUF023", "RUF024", "RUF026", "RUF028", "RUF029", "RUF030", "RUF031", "RUF032", "RUF033", "RUF034", "RUF100", "RUF101", "RUF200",
 ]
 ignore = ["E501", "E731"]
 [tool.ruff.lint.flake8-comprehensions]
 allow-dict-calls-with-keyword-arguments = true
+[tool.ruff.lint.flake8-type-checking]
+exempt-modules = ["typing", "collections.abc"]
 
 [tool.mypy]
 warn_unreachable = true

commit db2391d2c0b1eed0b62acf1a43951cb7ce37045f

Fix critical bug with detecting directories starting with `test_`
(Bug introduced in commit 02c51c35270dd7f797505a67b35e833d42ce6d80 / release 1.0.0)

Fixes #9

pytest_golden/plugin.py
@@ -407,7 +407,7 @@ def pytest_generate_tests(metafunc) -> None:
     rel_paths = [path.relative_to(directory) for path in paths]
     skip_parts = None
     if all(
-        "test_".removeprefix(path.parts[0]) == "test_".removeprefix(item.originalname)
+        path.parts[0].removeprefix("test_") == item.originalname.removeprefix("test_")
         for path in rel_paths
     ):
         skip_parts = 1